21142d34def4f27880853547afb9cdc0cbd44579
chiranjiviNeupane <crucialjudge@gmail.com>
2021-02-11 13:58:00 +1100

    hystrix dependency added and enabled hystrix on catalog

--- catalogservice/src/main/java/com/client/catalogservice/CatalogserviceApplication.java
+++ catalogservice/src/main/java/com/client/catalogservice/CatalogserviceApplication.java
@@ -2,15 +2,18 @@ package com.client.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class CatalogserviceApplication {
 
 	@Bean
 	@LoadBalanced
+
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
